d32355fbb6f6a6baf10868566a9f58ecd0b51b97
J. Taylor Allen <j.taylorallen@yahoo.com>
2022-02-03 13:33:51 -0600

    delete locks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -32,8 +32,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = 
-      'https://www.googleapis.com/books/v1/volumes?q=$(searchInput)';
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      );
 
       if (!response.ok) {
         throw new Error('something went wrong!');
